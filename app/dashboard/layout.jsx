b6c4b85eea64501c3abe57efa9dc183b9aeccef7
Muhammad Jazib <mjxdex@gmail.com>
2025-07-05 16:24:10 +0500

    prevent same loading again on route

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -36,22 +36,22 @@ const RootLayout = ({ children }) => {
                     {width <= 660 ? <X onClick={() => { setMenu(false) }} /> : <AlignJustify />}
                 </div>
                 <ul>
-                    <li><Link href="/dashboard/" className={pathname === "/dashboard" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><Home />Home</Link></li>
-                    <li><Link href="/dashboard/tracking" className={pathname === "/dashboard/tracking" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><MapPin />Tracking</Link></li>
-                    <li><Link href="/dashboard/archived" className={pathname === "/dashboard/archived" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><Archive />Archived</Link></li>
-                    <li><Link href="/dashboard/settings" className={pathname === "/dashboard/settings" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><Settings />Settings</Link></li>
+                    <li><Link href="/dashboard/" className={pathname === "/dashboard" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard" ? false : true); setMenu(false); }}><Home />Home</Link></li>
+                    <li><Link href="/dashboard/tracking" className={pathname === "/dashboard/tracking" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/tracking" ? false : true); setMenu(false); }}><MapPin />Tracking</Link></li>
+                    <li><Link href="/dashboard/archived" className={pathname === "/dashboard/archived" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/archived" ? false : true); setMenu(false); }}><Archive />Archived</Link></li>
+                    <li><Link href="/dashboard/settings" className={pathname === "/dashboard/settings" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/settings" ? false : true); setMenu(false); }}><Settings />Settings</Link></li>
                     {
                         menu && width <= 660 ? <>
-                            <li><Link href="/dashboard/shipment/new" className={pathname === "/dashboard/shipment/new" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><CirclePlus />New Shipment</Link></li>
-                            <li><Link href="/dashboard/notifications" className={pathname === "/dashboard/notifications" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}>{notifications.total >= 1 ? <BellDot /> : <Bell />}Notifications</Link></li>
-                            <li><Link href="/dashboard/profile" className={pathname === "/dashboard/profile" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><UserRound />Profile</Link></li>
+                            <li><Link href="/dashboard/shipment/new" className={pathname === "/dashboard/shipment/new" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/shipment/new" ? false : true); setMenu(false); }}><CirclePlus />New Shipment</Link></li>
+                            <li><Link href="/dashboard/notifications" className={pathname === "/dashboard/notifications" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/notifications" ? false : true); setMenu(false); }}>{notifications.total >= 1 ? <BellDot /> : <Bell />}Notifications</Link></li>
+                            <li><Link href="/dashboard/profile" className={pathname === "/dashboard/profile" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/profile" ? false : true); setMenu(false); }}><UserRound />Profile</Link></li>
                             {/* ... You can add more routes here for mobile menu only */}
                         </> : <></>
                     }
                 </ul>
                 <ul className='footer'>
-                    <li><Link href="/dashboard/subscription" className={pathname === "/dashboard/subscription" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><Crown />Subscription</Link></li>
-                    <li><Link href="/dashboard/support" className={pathname === "/dashboard/support" ? "active" : "inactive"} onClick={() => { setLoading(true); setMenu(false); }}><Info />Support</Link></li>
+                    <li><Link href="/dashboard/subscription" className={pathname === "/dashboard/subscription" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/subscription" ? false : true); setMenu(false); }}><Crown />Subscription</Link></li>
+                    <li><Link href="/dashboard/support" className={pathname === "/dashboard/support" ? "active" : "inactive"} onClick={() => { setLoading(pathname === "/dashboard/support" ? false : true); setMenu(false); }}><Info />Support</Link></li>
                     <li><Link href="/dashboard/" onClick={() => { setLoading(true); setMenu(false); }}><LogOut />Logout</Link></li>
                 </ul>
             </nav>
@@ -63,13 +63,13 @@ const RootLayout = ({ children }) => {
                         {menu ? <></> : <AlignJustify onClick={() => { setMenu(true) }} />}
                     </div>
                     <div className="col">
-                        <Link href="/dashboard/shipment/new" className='new-btn' onClick={() => { setLoading(true); setMenu(false); }}>New Shipment</Link>
+                        <Link href="/dashboard/shipment/new" className='new-btn' onClick={() => { setLoading(pathname === "/dashboard/shipment/new" ? false : true); setMenu(false); }}>New Shipment</Link>
                         <div className="btn-grp">
-                            <Link href="/dashboard/" onClick={() => { setLoading(true); setMenu(false); }}><MessageCircleMore /></Link>
-                            <Link href="/dashboard/notifications" onClick={() => { setLoading(true); setMenu(false); }}>{notifications.total >= 1 ? <BellDot /> : <Bell />}</Link>
-                            <Link href="/dashboard/" onClick={() => { setLoading(true); setMenu(false); }}><Earth /></Link>
+                            <Link href="/dashboard/" onClick={() => { setLoading(pathname === "/dashboard" ? false : true); setMenu(false); }}><MessageCircleMore /></Link>
+                            <Link href="/dashboard/notifications" onClick={() => { setLoading(pathname === "/dashboard/notifications" ? false : true); setMenu(false); }}>{notifications.total >= 1 ? <BellDot /> : <Bell />}</Link>
+                            <Link href="/dashboard/" onClick={() => { setLoading(pathname === "/dashboard" ? false : true); setMenu(false); }}><Earth /></Link>
                         </div>
-                        <Link href="/dashboard/profile" className='user-btn' onClick={() => { setLoading(true); setMenu(false); }}><Image src={"/user.png"} alt='user' width={40} height={40} /></Link>
+                        <Link href="/dashboard/profile" className='user-btn' onClick={() => { setLoading(pathname === "/dashboard/profile" ? false : true); setMenu(false); }}><Image src={"/user.png"} alt='user' width={40} height={40} /></Link>
                     </div>
                 </header>
                 <div className='view'>
